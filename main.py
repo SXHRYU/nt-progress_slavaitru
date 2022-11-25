bc62834dc75664d243f20b36d7c2885666b1afb0
Slava Maltsev <slavaxru@gmail.com>
2022-11-25 09:41:33 +0300

    updated table using Rich

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import typer
 from datetime import datetime
+import typer
+from rich.console import Console
+from rich.table import Table
 
+console = Console()
 app = typer.Typer()
 
 
@@ -11,7 +14,7 @@ def deposit(
             + "Format must include pennies after a 'dot', e.g.: 100.12 OR 0.99 OR 2222.00 OR 0.01."),
     description: str = typer.Option(default="ATM Deposit", help="description of a deposit action.")
     ):
-    """Deposits money to a given client."""
+    """Deposits money to a given client. Examle: deposit 123-NSiw0-X 15421.22"""
     print(f"{client_id} depositted ${amount} for '{description}'.")
 
 @app.command()
@@ -22,25 +25,18 @@ def withdraw(
             + "Format must include pennies after a 'dot', e.g.: `100.12` OR `0.99` OR `2222.00` OR `0.01`."),
     description: str = typer.Option(default="ATM Withdrawal", help="description of a withdrawal action.")
     ):
-    """Withdraws money from a given client."""
+    """Withdraws money from a given client. Examle: withdraw 092-VanR0-S 100009.01"""
     print(f"{client_id} withdrew ${amount} for '{description}'.")
 
 @app.command()
 def show_bank_statement(client_id: str, since: datetime = None, till: datetime = None):
-    print(
-            "----------------------------------------------------------------------------------------------------\n"
-        +   "| Date                | Description      | Withdrawals     | Deposits          | Balance           |\n"
-        +   "|---------------------|------------------|-----------------|-------------------|-------------------|\n"
-        +   "|                     | Previous balance |                 |                   |            $900.00|\n"
-        +   "|---------------------|------------------|-----------------|-------------------|-------------------|\n"
-        +   "|yyyy-mm-dd hh:mnmn:ss| ATM Deposit      |                 |            $100.00|            $100.00|\n"
-        +   "|yyyy-mm-dd hh:mnmn:ss| ATM Deposit      |                 |      $5_000_000.50|      $5_000_100.50|\n"
-        +   "|yyyy-mm-dd hh:mnmn:ss| ATM Withdrawal   |      $180_000.00|                   |      $4_820_100.50|\n"
-        +   "|---------------------|------------------|-----------------|-------------------|-------------------|\n"
-        +   "|                     | Totals           |      $180_000.00|      $5_000_100.50|      $4_821_000.50|\n"
-        +   "----------------------------------------------------------------------------------------------------\n"
-    )
-
+    table = Table("Date", "Description", "Withdrawals", "Deposits", "Balance")
+    table.add_row("", "Previous balance", "", "", "$900.00", end_section=True)
+    table.add_row("yyyy-mm-dd hh:mnmn:ss", "ATM Deposit", "$100.00", "$100.00")
+    table.add_row("yyyy-mm-dd hh:mnmn:ss", "ATM Deposit", "$5_000_000.50", "$5_000_100.50")
+    table.add_row("yyyy-mm-dd hh:mnmn:ss", "ATM Withdrawal", "$180_000.00", "", "$4_820_100.50", end_section=True)
+    table.add_row("", "Totals", "$180_000.00", "$5_000_100.50", "$4_821_000.50")
+    console.print(table)
 
 if __name__ == "__main__":
     app()
