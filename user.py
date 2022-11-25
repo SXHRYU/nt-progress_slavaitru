40b5a2cf9dcc4f3c1cd39463d82ec2f47b1b3724
Slava Maltsev <slavaxru@gmail.com>
2022-11-25 12:07:40 +0300

    updates to user and account

--- user.py
+++ user.py
@@ -1,14 +1,53 @@
 from datetime import datetime
+from typing import Self
 
+
+class AccountCreationError(Exception):
+    ...
 
 class User:
-    def __init__(self, id: str) -> None:
-        self.id = id
+    users: dict[str, Self] = {}
 
+    def __new__(cls, id: str) -> Self:
+        if id in cls.users:
+            print(f"User exists. Your variable points to {cls.users[id]}.")
+            return cls.users[id]
+        else:
+            return super().__new__(cls)
+
+    def __init__(self, id: str) -> None:
+        super().__init__()
+        self.id: str = id
+        self.users[id]: Self = self
+
+    def _set_account(self, account: "Account") -> None:
+        self.account: Account = account
+
+    def __repr__(self) -> str:
+        return f"User: id='{self.id}'"
 
 class Account:
-    def __init__(self, owner: User, balance: float = 0.00) -> None:
-        # self.owner = owner
-        self.balance: float = balance
-        self.history: list[tuple[datetime, str]] = []
+    accounts: dict[str, "User"] = {}
 
+    def __new__(cls, id: str, balance: float = 0.00, *, owner_id: str) -> Self:
+        acc_exists: bool = id in cls.accounts
+        client_has_acc: bool = hasattr(User.users[owner_id], "account")
+        if acc_exists:
+            raise AccountCreationError(f"Account exists.")
+        if client_has_acc:
+            raise AccountCreationError(f"This client already has account.")
+        return super().__new__(cls)
+
+    def __init__(self, id: str, balance: float = 0.00, *, owner_id: str) -> None:
+        super().__init__()
+        self.id: str = id
+        self.balance: float = balance
+        self.accounts[id]: Self = self
+
+        self.history: list[tuple[datetime, str]] = []
+            
+        self.owner: User = User.users[owner_id]
+        self.owner._set_account(self)
+
+    def __repr__(self) -> str:
+        return f"Account: id='{self.id}', owner='{self.owner}'"
